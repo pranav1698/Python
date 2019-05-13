394adac25f4eb68382e5afe6e6c653b4a76128b5
pranav1698 <hulkcodexl16@gmail.com>
2019-05-13 13:58:54 +0530

    This adds problems in python

--- Problems/Strings_and_Text/CombiningConcatenating.py
+++ Problems/Strings_and_Text/CombiningConcatenating.py
@@ -4,3 +4,7 @@ parts = ['Is', 'Chicago', 'Not', 'Chicago?']
 print ' '.join(parts)
 # Is Chicago Not Chicago?
 
+""" The + operator to joina a lot of strings is grossly ineffecient due to memory copies and garbage collection that occurs, """
+
+print(a, b , c, sep=':')
+
